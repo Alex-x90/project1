49d2d9ed6ad33d089e90c8fb9295d2787d2f0956
Sasha Nicholai Rosenthal <sar537@g.harvard.edu>
2018-07-12 14:29:46 +0000

    lots more bugfixes and getting things to work

--- application.py
+++ application.py
@@ -1,7 +1,16 @@
+#current bugs:
+#1) when you query it shows the exact output for your input, not ones that might be what you were searching for
+#2) searching with a city gives a lot of different cities which don't match the query
+#3) when you try to go to one of the information pages about a zipcode it gives you the error KeyError: 'currently' which is referring to the json.dumb somehow
+
+#It is currently unknown if checking in works
+
+
+
 import os
 import requests, json
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, jsonify, redirect, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -30,65 +39,79 @@ def index():
 
 @app.route("/register" , methods=["GET","POST"])
 def register():
-    return render_template("register.html")
+    if request.method == 'GET':
+        return render_template("register.html")
     if request.method == 'POST':
-        username = request.form("username")
-        password = request.form("password")
-        #gets username and password from form. Isn't working for some reason
-        print(username)
-        print(password)
+        username = request.form.get("username")
+        password = request.form.get("password")
+        #gets username and password from form.
 
-    #checks if that username is already in the database. if results are none it means it isnt and it tries to store the username and password entered. otherwise it gives an error. doesnt work
-    username_stored = db.execute("SELECT * FROM accounts WHERE username = :username", {"username": username}).fetchone()
-    print (username_stored)
-    if username_stored is None:
-        db.execute ("INSERT INTO accounts (username, password) VALUES (':username',':password')",{"username":username,"password":password})
+
+    #checks if that username is already in the database. if results are none it means it isnt and it tries to store the username and password entered. otherwise it gives an error.
+        if db.execute("SELECT * FROM accounts WHERE username = :username", {"username": username}).rowcount != 0:
+            return render_template("error.html", message="That username already exists")
+        db.execute("INSERT INTO accounts (username, password) VALUES (:username,:password)",{"username":username,"password":password})
         print("log")
         db.commit()
         return render_template("login.html")
 
-    else:
-        return render_template("error.html", message="That username already exists")
-
 @app.route("/main" , methods=["GET","POST"])
 def main():
-    session["account"] = 'admin' #this is for testing purposes
-
-    #renders the sight showing the area where the user can input data
+    if session.get("account") is None:
+        return render_template("error.html", message="You aren't logged in.")
     data_request = 1
-    return render_template("main.html", account = session["account"], data_request=data_request)
-    zipcode = request.form("zipcode")
+    #renders the site showing the area where the user can input data
+    if request.method == 'GET':
+        return render_template("main.html", account = session["account"], data_request=data_request)
+    if request.method == 'POST':
+        zipcode = request.form.get("zipcode")
+        city = request.form.get("city")
 
-    #when the user inputs a zipcode either tells them the zipcode doesnt exist or shows the information for that zipcode. for some reason when entering a zipcode the page just seems to reload
-    if db.execute("SELECT * FROM weather WHERE zipcode = :zipcode", {"zipcode": zipcode}).rowcount == 0:
-        return render_template("error_logged_in.html", message="sorry, that zipcode doesn't exist.")
-    else:
-        location = db.execute("SELECT * FROM weather WHERE zipcode = :zipcode;",{"zipcode":zipcode}).fetchone()
-        data_request = 0
-        lattitude = db.execute("SELECT lattitude FROM weather WHERE zipcode=:zipcode",{"zipcode":zipcode}).fetchone()
-        longitude = db.execute("SELECT longitude FROM weather WHERE zipcode=:zipcode",{"zipcode":zipcode}).fetchone()
-        weather = requests.get("https://api.darksky.net/forecast/f8440b78453c3b6fc21855d9a12c8276/:lattitude,:longitude",{"lattitude": lattitude, "longitude": longitude }).json()
-        #print(json.dumps(weather["currently"], indent = 2))
-        note = db.execute("SELECT * FROM check_in WHERE zipcode = :zipcode;",{"zipcode":zipcode}).fetchall()
+        #when the user inputs a zipcode either tells them the zipcode doesnt exist or shows the information for that zipcode.
+        if ((db.execute("SELECT * FROM weather WHERE zipcode LIKE :zipcode", {"zipcode": '%'+zipcode+'%'}).rowcount == 0) and (db.execute("SELECT * FROM weather WHERE city = :city", {"city": f"city"}).rowcount == 0)):
+            return render_template("error_logged_in.html", message="sorry, that location doesn't exist.")
+
+        location = db.execute("SELECT * FROM weather WHERE zipcode LIKE :zipcode",{"zipcode":'%'+zipcode+'%'}).fetchall()
+        if location == None:
+            location = db.execute("SELECT * FROM weather WHERE city = :city", {"city": f"%{city}%"}).fetchall()
+        return render_template("main.html", zipcodes=location,data_request=0,account = session["account"])
+
+
+@app.route("/main/<zipcode>")
+def main_data(zipcode):
+
+    if session.get("account") is None:
+        return render_template("error.html", message="You aren't logged in.")
+    location = db.execute("SELECT * FROM weather WHERE zipcode = :zipcode",{"zipcode":zipcode}).fetchone()
+    lattitude = db.execute("SELECT lattitude FROM weather WHERE zipcode=:zipcode",{"zipcode":zipcode}).fetchone()
+    longitude = db.execute("SELECT longitude FROM weather WHERE zipcode=:zipcode",{"zipcode":zipcode}).fetchone()
+    print (lattitude)
+    weather = str(requests.get("https://api.darksky.net/forecast/f8440b78453c3b6fc21855d9a12c8276/"+lattitude+','+longitude).json())
+    weather = (json.dumps(weather["currently"], indent = 2))
+    note = db.execute("SELECT note FROM check_in WHERE zipcode = :zipcode",{"zipcode":zipcode}).fetchall()
+    for x in note:
         notes.append(note)
 
-        #checks if the user has already made a note. if so doesn't display the portion of the page that would allow them to submit a new note.
-        if db.execute("SELECT * FROM check_in WHERE zipcode = :zipcode and username = :username", {"zipcode": zipcode,"username":session["account"]}).rowcount == 0:
-            no_comment = 1
-        else:
-            no_comment = 0
-        #re-renders main with all the data that has been added.
-        return render_template("main.html", location=location,notes=notes,no_comment=no_comment, data_request=data_request,weather=json.dumps(weather["currently"], indent = 2))
+    #checks if the user has already made a note. if so doesn't display the portion of the page that would allow them to submit a new note.
+    if db.execute("SELECT * FROM check_in WHERE zipcode = :zipcode and username = :username", {"zipcode": zipcode,"username":session["account"]}).rowcount == 0:
+        no_comment = 1
+    else:
+        no_comment = 0
+    #re-renders main with all the data that has been added.
+    return render_template("main.html", location=location,notes=notes,no_comment=no_comment,weather=weather)
 
-@app.route("/check_in" , methods=["GET","POST"])
-def check_in():
+@app.route("/check_in/<zipcode>" , methods=["GET","POST"])
+def check_in(zipcode):
+    if session.get("account") is None:
+        return render_template("error.html", message="You aren't logged in.")
     if request.method == "POST":
         check_in = request.form.get("check_in")
         if check_in:
         #checks if the user actually entered a note
             if db.execute("SELECT * FROM check_in WHERE zipcode = :zipcode and username = :username", {"zipcode": zipcode,"username":session["account"]}).rowcount == 0:
-                db.execute("INSERT INTO check_in (username,zipcode,note) VALUES (:username,:zipcode,:check_in"),{"username":session["account"],"check_in":check_in,"zipcode":zipcode})
+                db.execute("INSERT INTO check_in (username,zipcode,note) VALUES (:username,:zipcode,:check_in)",{"username":session["account"],"check_in":check_in,"zipcode":zipcode})
                 db.execute("UPDATE weather SET check_ins = check_ins + 1 WHERE zipcode = :zipcode",{"zipcode":zipcode})
+                db.commit()
                 #adds users check in to the list of check ins and lets the main database know someone has checked in at a zipcode
 
             else :
@@ -100,36 +123,49 @@ def check_in():
 @app.route("/logout")
 def logout():
     #logs the user out by deleting informtion from the session that stores their information
-    session["account"] = []
+    session["account"] = None
     return render_template("entrance.html")
 
 @app.route("/login" , methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
-
+    if request.method == 'GET':
+        return render_template("login.html")
+    if request.method == 'POST':
     #sets up the account session so that if the user logs in the website can store their username and display it.
-    if session.get("account") is None:
-        session["account"] = []
-    if request.method == "POST":
+
         username = request.form.get("username")
         password = request.form.get("password")
 
-    #checks if the users username and password match up with a username and password pair in the database. This doesn't work yet
-    if db.execute("SELECT * FROM accounts WHERE username = :username and password = :password", {"username": username,"password": password }).fetchone() != None
-        session["account"] = [username]
-        return render_template("main.html", account = session["account"])
-
-    else:
-        return render_template("error.html", message="That account doesn't exist")
+        #checks if the users username and password match up with a username and password pair in the database. This doesn't work yet
+        if db.execute("SELECT * FROM accounts WHERE username = :username and password = :password", {"username": username,"password": password }).rowcount == 0:
+            if db.execute("SELECT * FROM accounts WHERE username = :username ", {"username": username}).rowcount == 0:
+                return render_template("error.html", message="That account doesn't exist")
+            return render_template("error.html", message="You entered the incorrect password")
 
 
-@app.route("/api/<int:zipcode>")
-def request(zipcode):
+        else:
+            if session.get("account") is None:
+                session["account"] = []
+            data_request = 1
+            session["account"] = username
+            return render_template("main.html", account = session["account"],data_request=data_request)
+
+
+@app.route("/api/<zipcode>")
+def api_request(zipcode):
     #gets the information about the zipcode the user entered. Dont know how to properly set up the json output that is asked for
     if db.execute("SELECT * FROM weather WHERE zipcode = :zipcode", {"zipcode": zipcode}).rowcount == 0:
         return render_template("error.html", message="error 404: page not found")
     else:
-        location = db.execute("SELECT * FROM weather WHERE zipcode = :zipcode;",{"zipcode":zipcode}).fetchall()
-        return render_template("zipcodes.html", location=location)
+        location = db.execute("SELECT * FROM weather WHERE zipcode = :zipcode",{"zipcode":zipcode}).fetchone()
+        return jsonify({
+            "zipcode": location["zipcode"],
+            "city": location["city"],
+            "state": location["state"],
+            "lattitude": location["lattitude"],
+            "longitude": location["longitude"],
+            "population": location["population"],
+            "check_ins": location["check_ins"]
+        })
 
 #postgres://ufyogmokkpfcjp:474612b9cef13b1a1d7ddf8ce28951237aca7dece94e70bfb549deebda7be0b8@ec2-54-227-244-122.compute-1.amazonaws.com:5432/d8llvvbc60g38i
